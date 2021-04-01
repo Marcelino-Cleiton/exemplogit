2e206a6caff57c5723380afc15d2f7bf149a67c5
Cleiton Marcelino <cleitonmarcelino18@gmail.com>
2021-04-01 20:04:11 -0300

    Adicionado validacao de nome vaio no formulario

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function helloWord(){
     var inputs = document.getElementsByTagName('input');
     var input = inputs [0];
-    var nome  = input.value;
+    var nome  = input.value.trim();
+
+    if (nome === '' ) {
+        alert('O nome está vazio, por favor insira o nome.');
+        return;
+    }
     window.alert("Hello Word, " + nome);
     input.value='';
 }
